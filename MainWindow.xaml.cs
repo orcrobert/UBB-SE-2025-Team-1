52df1ea591d8185704f428903f9e1c6a7a137ed1
CyberNinjaH <robyorban@gmail.com>
2025-03-26 22:42:11 +0200

    lesgo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MySql.Data.MySqlClient;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,6 +24,8 @@ namespace WinUIApp
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        string connectionString = "Server=127.0.0.1;Database=DrinkMDb;User ID=;Password=;Port=3306;";
+        
         public MainWindow()
         {
             this.InitializeComponent();
@@ -31,6 +34,36 @@ namespace WinUIApp
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    myTextBlock.Text = "Connected to MySQL!";
+
+                    string query = "SELECT * FROM Brand;";
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                int brandId = reader.GetInt32(0);
+                                string brandName = reader.GetString(1);
+                                myTextBlock2.Text = $"Brand: {brandName} (ID: {brandId})";
+                            }
+                            else
+                            {
+                                myTextBlock2.Text = "No data found!";
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    myTextBlock.Text = $"Connection failed: {ex.Message}";
+                }
+            }
         }
     }
 }

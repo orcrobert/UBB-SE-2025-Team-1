57428769ff20761a53a1e9d65fc56b7531ec90af
CyberNinjaH <robyorban@gmail.com>
2025-03-27 13:39:36 +0200

    exec query/select (generalized)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using WinUIApp.Services;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace WinUIApp
 {
@@ -23,21 +24,19 @@ namespace WinUIApp
                 myTextBlock.Text = "Connected to MySQL!";
 
                 string query = "SELECT * FROM Brand;";
-                using (var command = new MySqlCommand(query, dbService.GetConnection()))
+                var selectResult = dbService.ExecuteSelect(query);
+
+                if (selectResult.Count > 0)
                 {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            int brandId = reader.GetInt32(0);
-                            string brandName = reader.GetString(1);
-                            myTextBlock2.Text = $"Brand: {brandName} (ID: {brandId})";
-                        }
-                        else
-                        {
-                            myTextBlock2.Text = "No data found!";
-                        }
-                    }
+                    var firstRow = selectResult[0];
+                    int brandId = Convert.ToInt32(firstRow["BrandId"]);
+                    string brandName = firstRow["BrandName"].ToString();
+
+                    myTextBlock2.Text = $"Brand: {brandName} (ID: {brandId})";
+                }
+                else
+                {
+                    myTextBlock2.Text = "No data found!";
                 }
             }
             catch (Exception ex)
--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -63,5 +63,75 @@ namespace WinUIApp.Services
                 _connection.Close();
             }
         }
+
+        public List<Dictionary<string, object>> ExecuteSelect(string query, List<MySqlParameter> parameters = null)
+        {
+            var result = new List<Dictionary<string, object>>();
+
+            try
+            {
+                OpenConnection();
+
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new Dictionary<string, object>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[reader.GetName(i)] = reader.GetValue(i);
+                            }
+                            result.Add(row);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return result;
+        }
+
+        public int ExecuteQuery(string query, List<MySqlParameter> parameters = null)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                OpenConnection();
+
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters.ToArray());
+                    }
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return rowsAffected;
+        }
     }
 }

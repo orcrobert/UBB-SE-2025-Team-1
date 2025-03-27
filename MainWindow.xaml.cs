2d6ae81b5cdfdedfad775c2356146c114019fe11
CyberNinjaH <robyorban@gmail.com>
2025-03-27 12:52:53 +0200

    aaa

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,40 +1,30 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Configuration;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using MySql.Data.MySqlClient;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace WinUIApp
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
-        string connectionString = "Server=127.0.0.1;Database=DrinkMDb;User ID=;Password=;Port=3306;";
-        
+        private readonly string _connectionString;
+
         public MainWindow()
         {
+            _connectionString = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
             this.InitializeComponent();
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
-            using (var connection = new MySqlConnection(connectionString))
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                myTextBlock.Text = "Connection string is not loaded!";
+                return;
+            }
+
+            using (var connection = new MySqlConnection(_connectionString))
             {
                 try
                 {

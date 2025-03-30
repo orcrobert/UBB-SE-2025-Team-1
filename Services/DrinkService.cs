4739506f53aeb766e83c4581915841c2d8b5b527
Mol-Anita <molanita1121@gmail.com>
2025-03-31 01:18:21 +0300

    core: created the class and get, add, delete, update method headers

---
+++ Services/DrinkService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinUIApp.Models;
+
+
+namespace WinUIApp.Services
+{
+    class DrinkService
+    {
+        public DrinkService() { }
+
+        public List<Drink> getDrinks()
+        {
+            List<Drink> drinks = [];
+            return drinks;
+        }
+
+        public void addDrink(List<Category> categories, string brandName, float alcoholContent)
+        {
+        }
+
+        public void updateDrink(Drink drink)
+        {
+        }
+
+        public void deleteDrink(int drinkId)
+        {
+        }
+    }
+}

b3dd8c017cd60c1eca73d9befa68823b7a59805e
Mol-Anita <molanita1121@gmail.com>
2025-03-31 12:21:18 +0300

    feat: implemented all basic crud operations

--- Services/DrinkService.cs
+++ Services/DrinkService.cs
@@ -10,24 +10,83 @@ namespace WinUIApp.Services
 {
     class DrinkService
     {
-        public DrinkService() { }
-
-        public List<Drink> getDrinks()
+        private DrinkModel drinkModel;
+        public DrinkService()
         {
-            List<Drink> drinks = [];
-            return drinks;
+            drinkModel = new DrinkModel();
+        }
+
+        public List<Drink> getDrinks(string? searchedTerm, List<string>? brandNameFilter, List<string>? categoryFilter, float? minAlcohol, float? maxAlcohol, Dictionary<string, bool>? orderBy)
+        {
+            try
+            {
+                return drinkModel.getDrinks(searchedTerm, brandNameFilter, categoryFilter, minAlcohol, maxAlcohol, orderBy);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while getting drinks:", e);
+            }
         }
 
         public void addDrink(List<Category> categories, string brandName, float alcoholContent)
         {
+            try
+            {
+                drinkModel.addDrink(categories, brandName, alcoholContent);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while adding a drink:", e);
+            }
         }
 
         public void updateDrink(Drink drink)
         {
+            try
+            {
+                drinkModel.updateDrink(drink);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while updating a drink:", e);
+            }
         }
 
         public void deleteDrink(int drinkId)
         {
+            try
+            {
+                drinkModel.deleteDrink(drinkId);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while deleting a drink:", e);
+            }
+        }
+
+        public List<Category> getDrinkCategories()
+        {
+            try
+            {
+                return drinkModel.getDrinkCategories();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while getting drink categories:", e);
+            }
+        }
+
+        public List<Brand> getDrinkBrands()
+        {
+            try
+            {
+                return drinkModel.getDrinkBrands();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error happened while getting drink brands:", e);
+            }
         }
     }
 }

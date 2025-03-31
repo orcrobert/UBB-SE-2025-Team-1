c837d24bd0163582752b890a34da9b5463f723b5
915-Paulovici-Anndrei <itsfazzeliv2@gmail.com>
2025-03-31 20:18:24 +0300

    Populated the form with sample data, and deleted the description field + fixed one bug

--- Views/Components/UpdateDrinkButton.xaml.cs
+++ Views/Components/UpdateDrinkButton.xaml.cs
@@ -1,19 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Text;
-using Microsoft.UI;
+using WinUIApp.Models;
 
 namespace WinUIApp.Views.Components
 {
@@ -26,13 +19,26 @@ namespace WinUIApp.Views.Components
 
         private void UpdateDrinkButton_Click(object sender, RoutedEventArgs e)
         {
+            var testDrink = new Drink(
+                id: 1,
+                categories: new List<Category>
+                {
+                    new Category(101, "Beer"),
+                    new Category(102, "Cocktail")
+                },
+                brand: new Brand(201, "TestBrand"),
+                alcoholContent: 5.5f
+            );
+
             var flyout = new Flyout
             {
-                Content = new UpdateDrinkFlyout()
+                Content = new UpdateDrinkFlyout
+                {
+                    DrinkToUpdate = testDrink
+                }
             };
 
             flyout.ShowAt(UpdateButton);
         }
-
     }
 }
--- Views/Components/UpdateDrinkFlyout.xaml.cs
+++ Views/Components/UpdateDrinkFlyout.xaml.cs
@@ -1,63 +1,135 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WinUIApp.Models;
+using WinUIApp.Services;
 
 namespace WinUIApp.Views.Components
 {
     public sealed partial class UpdateDrinkFlyout : UserControl
     {
-        private List<string> _allCategories = new()
+        public Drink DrinkToUpdate { get; set; }
+
+        private readonly List<string> _allCategories = new()
         {
             "Beer", "Wine", "Whiskey", "Vodka", "Cocktail", "Juice", "Cider", "Soft Drink"
         };
 
+        private readonly HashSet<string> _selectedCategoryNames = new();
+
         public UpdateDrinkFlyout()
         {
             this.InitializeComponent();
+
             CategoryList.ItemsSource = new List<string>(_allCategories);
+            CategoryList.SelectionMode = ListViewSelectionMode.Multiple;
+
+            CategoryList.SelectionChanged += CategoryList_SelectionChanged;
 
             SearchBox.TextChanged += (s, e) =>
             {
-                var query = SearchBox.Text.ToLower();
-                var selected = CategoryList.SelectedItems.Cast<string>().ToList();
+                string query = SearchBox.Text.ToLower();
 
                 var filtered = _allCategories
                     .Where(c => c.ToLower().Contains(query))
                     .ToList();
 
+                CategoryList.SelectionChanged -= CategoryList_SelectionChanged;
+
                 CategoryList.ItemsSource = filtered;
 
-                foreach (var item in selected)
-                    if (filtered.Contains(item))
-                        CategoryList.SelectedItems.Add(item);
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    foreach (var item in filtered)
+                    {
+                        if (_selectedCategoryNames.Contains(item))
+                        {
+                            CategoryList.SelectedItems.Add(item);
+                        }
+                    }
+
+                    CategoryList.SelectionChanged += CategoryList_SelectionChanged;
+                });
             };
+
+            this.Loaded += UpdateDrinkFlyout_Loaded;
+        }
+
+        private void CategoryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var removed in e.RemovedItems.Cast<string>())
+                _selectedCategoryNames.Remove(removed);
+
+            foreach (var added in e.AddedItems.Cast<string>())
+                _selectedCategoryNames.Add(added);
+        }
+
+        private void UpdateDrinkFlyout_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DrinkToUpdate != null)
+            {
+                BrandBox.Text = DrinkToUpdate.Brand?.Name ?? "";
+                AlcoholBox.Text = DrinkToUpdate.AlcoholContent.ToString();
+
+                var selectedNames = DrinkToUpdate.Categories.Select(c => c.Name).ToList();
+                foreach (var name in selectedNames)
+                {
+                    _selectedCategoryNames.Add(name);
+                }
+
+                CategoryList.ItemsSource = _allCategories;
+
+                foreach (var item in _allCategories)
+                {
+                    if (_selectedCategoryNames.Contains(item))
+                        CategoryList.SelectedItems.Add(item);
+                }
+            }
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            var selected = CategoryList.SelectedItems.Cast<string>().ToList();
-            var dialog = new ContentDialog
+            if (DrinkToUpdate != null)
             {
-                Title = "Selected Categories",
-                Content = string.Join(", ", selected),
-                CloseButtonText = "OK",
-                XamlRoot = this.XamlRoot
-            };
-            _ = dialog.ShowAsync();
+                try
+                {
+                    DrinkToUpdate.Brand = new Brand(1, BrandBox.Text);
+
+                    if (float.TryParse(AlcoholBox.Text, out var alc))
+                        DrinkToUpdate.AlcoholContent = alc;
+
+                    DrinkToUpdate.Categories = _selectedCategoryNames
+                        .Select(name => new Category(1, name))
+                        .ToList();
+
+                    var service = new DrinkService();
+                    service.updateDrink(DrinkToUpdate);
+
+                    var dialog = new ContentDialog
+                    {
+                        Title = "Success",
+                        Content = "Drink updated successfully.",
+                        CloseButtonText = "OK",
+                        XamlRoot = this.XamlRoot
+                    };
+                    _ = dialog.ShowAsync();
+                }
+                catch (Exception ex)
+                {
+                    var dialog = new ContentDialog
+                    {
+                        Title = "Error",
+                        Content = ex.Message,
+                        CloseButtonText = "OK",
+                        XamlRoot = this.XamlRoot
+                    };
+                    _ = dialog.ShowAsync();
+                }
+            }
         }
     }
 }

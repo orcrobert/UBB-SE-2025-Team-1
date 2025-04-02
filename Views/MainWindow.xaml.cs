a2c9bcd9d82b40a08c51f3d14f6496da45b64f92
antoniunegrea <toninegrea12@yahoo.com>
2025-04-02 23:40:43 +0300

    .

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -3,17 +3,6 @@ using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;using WinUIApp.Services;
-using WinUIApp.Services.DummyServies;
-using WinUIApp.Views.Pages;
-using WinUIApp.Views.ViewModels;
 
 namespace WinUIApp.Views
 {

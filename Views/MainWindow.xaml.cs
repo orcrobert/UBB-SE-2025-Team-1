465f57c9e3e8313c9f7bfde1f4af7e1d07d51a59
orcrobert <robyorban@gmail.com>
2025-04-02 18:58:28 +0300

    mainpageviewmodel for the pdl component

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,37 +1,35 @@
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
 using System;
-using Windows.System;
-using WinUIApp.Models;
-using WinUIApp.Services;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;using WinUIApp.Services;
 using WinUIApp.Services.DummyServies;
-using WinUIApp.Views.ModelViews;
 using WinUIApp.Views.Pages;
+using WinUIApp.Views.ViewModels;
 
 namespace WinUIApp.Views
 {
     public sealed partial class MainWindow : Window
     {
+        public MainPageViewModel ViewModel { get; } = new MainPageViewModel();
 
         public MainWindow()
         {
             this.InitializeComponent();
-            //SetFixedSize(1440, 900);
-            //DrinkService drinkService = new DrinkService();
-            //ReviewService reviewService = new ReviewService();
-            //SearchPageViewModel searchPageViewModel = new SearchPageViewModel(MainFrame, drinkService, reviewService);
-            //MainFrame.Navigate(typeof(SearchPage), searchPageViewModel);
         }
 
-        //private void SetFixedSize(int width, int height)
-        //{
-        //    IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-        //    var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
-        //    var appWindow = AppWindow.GetFromWindowId(windowId);
-
-        //    appWindow.Resize(new Windows.Graphics.SizeInt32(width, height));
-        //}
-
+        // You can keep or remove the SetFixedSize and other commented-out code as needed
+        // private void SetFixedSize(int width, int height)
+        // {
+        //     IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        //     var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        //     var appWindow = AppWindow.GetFromWindowId(windowId);
+        //
+        //     appWindow.Resize(new Windows.Graphics.SizeInt32(width, height));
+        // }
     }
 }

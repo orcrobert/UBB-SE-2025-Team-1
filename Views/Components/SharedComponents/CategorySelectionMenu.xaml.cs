fd36ac3485836101b51ded0a080c4ead69e45f57
915-Pangaleanu-Catalin <146840522+915-Pangaleanu-Catalin@users.noreply.github.com>
2025-04-02 14:36:52 +0300

    Added prototype of category selection menu inside shared coponents folder.

---
+++ Views/Components/SharedComponents/CategorySelectionMenu.xaml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using WinUIApp.Models;
+
+namespace WinUIApp.Views.Components.SharedComponents
+{
+    public sealed partial class CategorySelectionMenu : UserControl
+    {
+        public List<Category> Categories { get; set; }
+
+        public CategorySelectionMenu()
+        {
+            this.InitializeComponent();
+            populateCategories();
+        }
+
+        private void populateCategories()
+        {
+            Categories = new List<Category> { new Category (1, "abc") };
+        }
+    }
+}

d6e228aa2bfe09f747bced39bc66b72f21a6dea7
Mol-Anita <molanita1121@gmail.com>
2025-04-02 20:43:47 +0300

    refact: moved ViewModels folder to root directory

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinUIApp.Models;
+using WinUIApp.Services;
+using WinUIApp.Services.DummyServies;
+
+namespace WinUIApp.ViewModels
+{
+    class MainPageViewModel 
+    {
+        private DrinkService drinkService = new DrinkService();
+        private UserService userService = new UserService();
+
+        public MainPageViewModel()
+        {
+        }
+
+        public Drink getDrinkOfTheDay()
+        {
+            return drinkService.getDrinkOfTheDay();
+        }
+    }
+    
+    
+}

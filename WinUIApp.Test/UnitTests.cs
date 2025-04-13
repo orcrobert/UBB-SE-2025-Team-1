cee7ab87cdab4a9a3922949e54450383b0cd49f8
alexzmmv <constantin.alexandru.dumitrascu@stud.ubbcluj.ro>
2025-04-13 19:26:36 +0300

    test the tests and aded tests project

---
+++ WinUIApp.Test/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WinUIApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}

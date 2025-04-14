086567a87f3e299a6eb62b357d4a4c01f8ef5f67
Feketics <feketicshunor@gmail.com>
2025-04-14 15:59:21 +0300

    Refactor/ UserService interface

--- MainProject/Services/DummyServices/IUserService.cs
+++ MainProject/Services/DummyServices/IUserService.cs
@@ -1,9 +1,23 @@
-﻿namespace WinUIApp.Services.DummyServices
+﻿// <copyright file="IUserService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WinUIApp.Services.DummyServices
 {
+    /// <summary>
+    /// Interface for managing user-related operations.
+    /// </summary>
     public interface IUserService
     {
+        /// <summary>
+        /// Gets the current user ID.
+        /// </summary>
         int CurrentUserId { get; }
 
+        /// <summary>
+        /// Gets the current user id.
+        /// </summary>
+        /// <returns> User id. </returns>
         int GetCurrentUserId();
     }
 }

f5eb83a72ab6dc10f5e46c8e7b3909d5c8456cc3
Feketics <feketicshunor@gmail.com>
2025-04-14 16:34:49 +0300

    Refactor/ Page navigation

--- MainProject/Utils/NavigationParameters/SearchPageNavigationParameters.cs
+++ MainProject/Utils/NavigationParameters/SearchPageNavigationParameters.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections.Generic;
-using WinUIApp.Models;
+﻿// <copyright file="SearchPageNavigationParameters.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WinUIApp.Utils.NavigationParameters
 {
+    using System.Collections.Generic;
+    using WinUIApp.Models;
+
+    /// <summary>
+    /// Parameters for navigating to the search page.
+    /// </summary>
     public class SearchPageNavigationParameters
     {
+        /// <summary>
+        /// Gets or sets the list of selected brands for filtering.
+        /// </summary>
         public List<Category>? SelectedCategoryFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of selected categories for filtering.
+        /// </summary>
         public string? InputSearchKeyword { get; set; }
     }
 }

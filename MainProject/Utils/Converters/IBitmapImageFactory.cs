e03a87aa693c9b39aa912186a36aa45c678a164c
Feketics <feketicshunor@gmail.com>
2025-04-14 22:18:21 +0300

    Made everything to work together nicely

--- MainProject/Utils/Converters/IBitmapImageFactory.cs
+++ MainProject/Utils/Converters/IBitmapImageFactory.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.UI.Xaml.Media.Imaging;
-using System;
+﻿// <copyright file="IBitmapImageFactory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WinUIApp.Utils.Converters
 {
+    using System;
+    using Microsoft.UI.Xaml.Media.Imaging;
+
     /// <summary>
     /// Provides a factory interface to create BitmapImage instances.
     /// </summary>
     public interface IBitmapImageFactory
     {
+        /// <summary>
+        /// Creates a BitmapImage from a given URI.
+        /// </summary>
+        /// <param name="uri"> Uri. </param>
+        /// <returns>Bitmap image.</returns>
         BitmapImage Create(string uri);
     }
 }

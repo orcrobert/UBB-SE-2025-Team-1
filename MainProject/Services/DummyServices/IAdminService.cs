4d7dbf4aa85b0c03b1c7dbf97d5abb3ed5902464
Feketics <feketicshunor@gmail.com>
2025-04-14 15:56:26 +0300

    Refactor/ AdminService interface

--- MainProject/Services/DummyServices/IAdminService.cs
+++ MainProject/Services/DummyServices/IAdminService.cs
@@ -1,8 +1,27 @@
-﻿namespace WinUIApp.Services.DummyServices
+﻿// <copyright file="IAdminService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WinUIApp.Services.DummyServices
 {
+    /// <summary>
+    /// Interface for managing admin-related operations.
+    /// </summary>
     public interface IAdminService
     {
-         abstract bool IsAdmin(int userId);
-         abstract void SendNotificationFromUserToAdmin(int senderUserId, string userModificationRequestType, string userModificationRequestDetails);
+        /// <summary>
+        /// Checks if a user is an admin.
+        /// </summary>
+        /// <param name="userId"> User id. </param>
+        /// <returns> True, if admin, false otherwise. </returns>
+        abstract bool IsAdmin(int userId);
+
+        /// <summary>
+        /// Sends a notification from a user to an admin.
+        /// </summary>
+        /// <param name="senderUserId"> User id. </param>
+        /// <param name="userModificationRequestType"> Request type. </param>
+        /// <param name="userModificationRequestDetails"> Request details. </param>
+        abstract void SendNotificationFromUserToAdmin(int senderUserId, string userModificationRequestType, string userModificationRequestDetails);
     }
 }

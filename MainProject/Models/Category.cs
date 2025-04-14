974b40b60872fc80ecc27a180c5b5b27d1e17ba9
Feketics <feketicshunor@gmail.com>
2025-04-14 15:02:13 +0300

    Refactor/ Category

--- MainProject/Models/Category.cs
+++ MainProject/Models/Category.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿// <copyright file="Category.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WinUIApp.Models
 {
+    using System;
+
     /// <summary>
     /// Represents a category assigned to a drink (e.g., beer, wine).
     /// </summary>
     public class Category
     {
+        
+        
         /// <summary>
         /// Gets the unique identifier for the category.
         /// </summary>
@@ -16,21 +22,6 @@ namespace WinUIApp.Models
         /// Gets the name of the category.
         /// </summary>
         public string CategoryName { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Category"/> class.
-        /// </summary>
-        /// <param name="categoryId">Unique identifier for the category.</param>
-        /// <param name="categoryName">Name of the category.</param>
-        /// <exception cref="ArgumentException">Thrown when categoryName is null or empty.</exception>
-        public Category(int categoryId, string categoryName)
-        {
-            if (string.IsNullOrWhiteSpace(categoryName))
-                throw new ArgumentException("Category name cannot be null or empty.", nameof(categoryName));
-
-            CategoryId = categoryId;
-            CategoryName = categoryName;
-        }
 
         public override bool Equals(object? obj)
         {

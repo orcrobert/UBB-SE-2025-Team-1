5d620ba640bb8fa8a81bc9d40744fa6b126a712d
Feketics <feketicshunor@gmail.com>
2025-04-14 16:00:33 +0300

    Refactor/ UserService

--- MainProject/Services/DummyServices/UserService.cs
+++ MainProject/Services/DummyServices/UserService.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="UserService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WinUIApp.Services.DummyServices
 {
+    /// <summary>
+    /// Service for managing user-related operations.
+    /// </summary>
     public class UserService : IUserService
     {
         private const int DefaultUserId = 1;
+
+        /// <summary>
+        /// Gets the current user ID.
+        /// </summary>
         public int CurrentUserId { get; } = DefaultUserId;
 
+        /// <summary>
+        /// Gets the current user id.
+        /// </summary>
+        /// <returns> User id. </returns>
         public int GetCurrentUserId()
         {
-            return CurrentUserId;
+            return this.CurrentUserId;
         }
     }
 }
